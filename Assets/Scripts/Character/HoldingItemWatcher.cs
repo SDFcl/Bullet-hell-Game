03922d359d6b0c5da44aaa3c8c94ce72ad79566f
RuminaYuki <donminecraft36@gmail.com>
2026-04-10 13:50:21 +0700

    Fixed DebugScript

--- Assets/Scripts/Character/HoldingItemWatcher.cs
+++ Assets/Scripts/Character/HoldingItemWatcher.cs
@@ -6,7 +6,7 @@ public class HoldingItemWatcher : MonoBehaviour
     public event Action OnHoldingItemChanged;
     private void OnTransformChildrenChanged()
     {
-        Debug.Log("HoldingItem children changed");
+        //Debug.Log("HoldingItem children changed");
         OnHoldingItemChanged?.Invoke();
     }
 }
