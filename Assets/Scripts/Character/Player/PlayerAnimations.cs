d2c796693f43783d246040bd69ef53af6ad6b745
RuminaYuki <donminecraft36@gmail.com>
2026-04-14 21:56:10 +0700

    Add PlayerIFrame

--- Assets/Scripts/Character/Player/PlayerAnimations.cs
+++ Assets/Scripts/Character/Player/PlayerAnimations.cs
@@ -3,24 +3,33 @@ using UnityEngine;
 public class PlayerAnimations : CharacterAnimations
 {
     private Dodge dodge;
+    private PlayerHealth playerHealth;
 
     protected override void Awake()
     {
         base.Awake();
         dodge = GetComponent<Dodge>();
+        playerHealth = GetComponent<PlayerHealth>();
     }
     protected override void OnEnable()
     {
         base.OnEnable();
         dodge.OnDodge += TriggerDodgeAnimation;
+        playerHealth.OnIFrame += TriggerIFrameAnimation;
     }
     protected override void OnDisable()
     {
         base.OnDisable();
         dodge.OnDodge -= TriggerDodgeAnimation;
+        playerHealth.OnIFrame -= TriggerIFrameAnimation;
     }
     public void TriggerDodgeAnimation()
     {
         animator.SetTrigger("Dodge");
+    }
+
+    public void TriggerIFrameAnimation(bool OnIFrame)
+    {
+        animator.SetBool("OnIFrame",OnIFrame);
     }
 }
--- Assets/Scripts/Character/Player/PlayerHealth.cs
+++ Assets/Scripts/Character/Player/PlayerHealth.cs
@@ -1,7 +1,33 @@
+using System;
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : Health, IHealable
 {
+    [SerializeField] private float iframeDuration;
+    private Coroutine iFrameRoutine;
+    public Action<bool> OnIFrame;
+
+    public override void TakeDamage(float damage)
+    {
+        base.TakeDamage(damage);
+        if (iFrameRoutine != null || ignoreDamage || IsDead) return;
+        iFrameRoutine = StartCoroutine(IFrameCount());
+    }
+
+    IEnumerator IFrameCount()
+    {
+        EnableIgnoreDamage(true);
+        OnIFrame?.Invoke(true);
+
+        yield return new WaitForSeconds(iframeDuration);
+        //Debug.Log("Player can take damage");
+
+        EnableIgnoreDamage(false);
+        OnIFrame?.Invoke(false);
+        
+        iFrameRoutine = null;
+    }
     public void Heal(float amount)
     {
         if (IsDead) return;
