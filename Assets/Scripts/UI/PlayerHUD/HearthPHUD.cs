93826fe2c8f8108662c7d3a7576e9f921068fc75
RuminaYuki <donminecraft36@gmail.com>
2026-04-22 19:26:43 +0700

    Change PlayerPrefab

---
+++ Assets/Scripts/UI/PlayerHUD/HearthPHUD.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HearthPHUD : MonoBehaviour
+{
+    [SerializeField] private PlayerHealth playerHealth;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite emptyHeart;
+    [SerializeField] private float hpPerHeart = 1f;
+
+    private Image[] hearts;
+
+    void Awake()
+    {
+        if (playerHealth == null)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player != null)
+                playerHealth = player.GetComponent<PlayerHealth>();
+        }
+    }
+
+    void OnEnable()
+    {
+        if (playerHealth != null)
+            playerHealth.OnHealthChanged += UpdateHearts;
+    }
+
+    void Start()
+    {
+        hearts = GetComponentsInChildren<Image>();
+        UpdateHearts(playerHealth.CurrentHP);
+    }
+
+    void OnDisable()
+    {
+        if (playerHealth != null)
+            playerHealth.OnHealthChanged -= UpdateHearts;
+    }
+
+    void UpdateHearts(float currentHP)
+    {
+        if (hearts == null)
+            return;
+
+        int fullHeartCount = Mathf.CeilToInt(currentHP / hpPerHeart);
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].sprite = i < fullHeartCount ? fullHeart : emptyHeart;
+        }
+    }
+}
