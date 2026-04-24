f1da108914fb8c7c4daa228b53c535888855107a
RuminaYuki <donminecraft36@gmail.com>
2026-04-25 00:09:07 +0700

    ChangeCursor

---
+++ Assets/Scripts/UI/AimTargetFollowMouse.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AimTargetFollowMouse : MonoBehaviour
+{
+    private Camera inputCamera;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        if(inputCamera == null)
+        {
+            inputCamera = GameObject.FindGameObjectWithTag("Input Camera").GetComponent<Camera>();
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (inputCamera == null) return;
+
+        Vector3 mouseWorld = inputCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        mouseWorld.z = 0f;
+        transform.position = mouseWorld;
+    }
+
+    void OnEnable()
+    {
+        EventBus.Subscribe<GameStateChangedEvent>(EnableMyself);
+    }
+    void OnDisable()
+    {
+        EventBus.Unsubscribe<GameStateChangedEvent>(EnableMyself);
+    }
+
+    void EnableMyself(GameStateChangedEvent e)
+    {
+        if(e.NewState == GameState.GamePlay)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Confined;
+            spriteRenderer.color = new Color(1,1,1,1);
+        }
+        if(e.NewState == GameState.Paused)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            spriteRenderer.color = new Color(1,1,1,0);
+        }
+    }
+}
